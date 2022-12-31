e2510b833e4ea28e640ac11f905c1c47bd5f7828
bcapuano <bcapuano@asu.edu>
2022-12-30 20:48:37 -0500

    Initial Mayhem Heroes Integration

---
+++ mayhem/fuzz_reader.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+import atheris
+import sys
+import fuzz_helpers
+
+with atheris.instrument_imports(include=["pyembroidery"]):
+    import pyembroidery
+
+supported_file_suffxes = [
+    '.pes', '.dst', '.exp',
+    '.jef', '.vp3', '.10o',
+    '.100', '.bro', '.dat',
+    '.dsb', 'dsz', '.emd',
+    '.exy', '.fxy', '.gt',
+    '.hus', '.inb', '.jpx',
+    '.ksm', '.max', '.mit',
+    '.new', '.pcd', '.pcm',
+    '.pcq', '.pcs', '.pec',
+    '.phb', '.phc', '.sew',
+    '.shv', '.stc', '.stx',
+    '.tap', '.tbf', '.tbf',
+    '.u01', '.xxx', '.zhs',
+    '.xzy', '.gcode'
+]
+
+def TestOneInput(data):
+    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+    try:
+        with fdp.ConsumeTemporaryFile(suffix=fdp.PickValueInList(supported_file_suffxes), all_data=True) as file_path:
+            pyembroidery.read(file_path)
+    except TypeError:
+        # Raised too often
+        return -1
+
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
